3d193903e02f12cf39d1071847f465cb5b3f401d
zr123ccc <114324693+zr123ccc@users.noreply.github.com>
2022-09-25 12:57:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,13 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: '
+wx5a5fd1f414647ee6',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '4c50f85600beb3838739ef619e0c0296',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -60,20 +61,20 @@ const USER_CONFIG = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [odKrO6q-wblBu-RkolsVhUzvsLds,odKrO6glWztwrFIg-BaXEyXZYE9Y,odKrO6gT5v3VP6KJ6U3Eyil2AMBI
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '宝宝',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'odKrO6q-wblBu-RkolsVhUzvsLds',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Ua8cn_lG8pH-Y3zDOUusveO2NCNXRAC2F42NrdpSbj0',
       // 所在省份
-      province: '广东',
+      province: '甘肃',
       // 所在城市
-      city: '肇庆',
+      city: '兰州',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-31',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,17 +83,17 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '宝宝', year: '1995', date: '12-31',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '第一次加V', year: '2022', date: '08-12',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '爸爸', year: '1994', date: '10-30',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+          type: '节日', name: '第一次见面', year: '2022', date: '08-14',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
